919a9e9200cb6f655e46870ddf9116bbae9a5402
agabor <angyalgabor@outlook.com>
2017-09-11 16:53:40 +0200

    qt init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DicomReader
+# DicomReader #
+
+## Install prerequisites ##
+
+sudo apt-get install build-essential cmake libdcmtk-dev libopencv-dev qtbase5-dev
--- main.cpp
+++ main.cpp
@@ -3,17 +3,35 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "DicomReader.h"
 #include <iostream>
+#include <qt5/QtWidgets/QApplication>
+#include <qt5/QtWidgets/QMainWindow>
+#include <qt5/QtWidgets/QLabel>
+#include <qt5/QtWidgets/QLayout>
+#include <qt5/QtWidgets/QVBoxLayout>
+#include <qt5/QtGui/QImage>
 
-int main() {
+int main(int argc, char** argv)
+{
 
     std::cout << "open cv " << CV_MAJOR_VERSION << '.' << CV_MINOR_VERSION << std::endl;
     DicomReader r;
     r.addFile("../test.dcm");
     r.config();
-    cv::Mat image = r.read(CV_64F)[0];
+    cv::Mat image = r.read(CV_8U)[0];
 
     cv::namedWindow("test");
     cv::imshow("test", image);
-    cv::waitKey(0);
-    return 0;
+    //cv::waitKey(0);
+    QApplication app(argc, argv);
+    QMainWindow window;
+    window.setMinimumSize(image.cols, image.rows);
+    QLabel *label = new QLabel;
+    label->setFixedSize(image.cols, image.rows);
+    window.setLayout(new QVBoxLayout);
+    window.layout()->addWidget(label);
+
+    label->setPixmap(QPixmap::fromImage(QImage(image.data, image.cols, image.rows, int(image.step), QImage::Format_Grayscale8)));
+
+    window.show();
+    return app.exec();
 }
