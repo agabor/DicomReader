fd0d1b2eb4f0d8c8e11b349ca7753579e15694a1
agabor <angyalgabor@outlook.com>
2017-09-13 07:37:37 +0200

    match features

--- main.cpp
+++ main.cpp
@@ -25,13 +25,16 @@ public:
         r.addFile(file_name);
         r.config();
         mat= r.read(CV_8U)[0];
+
     }
 
-    void resize(const Image &templ) {
-        double m = (templ.mat.rows * templ.mat.cols) / (mat.rows * mat.cols);
+
+        void resize(const Image &templ) {
+        double m = sqrt(double(templ.mat.rows * templ.mat.cols) / double(mat.rows * mat.cols));
         Mat dest(static_cast<int>(mat.rows * m), static_cast<int>(mat.cols * m), CV_8U);
         cv::resize(mat, dest, dest.size());
         mat = dest;
+           cout <<  double(templ.mat.rows * templ.mat.cols) / double(mat.rows * mat.cols) << endl;
     }
 
     void scan(cv::SurfFeatureDetector &detector){
@@ -41,15 +44,32 @@ public:
         float min = 100000;
         float max = 0;
 
+        double ravg = 0;
+        float rmin = 100000;
+        float rmax = 0;
+
+        sort(keypoints.begin(), keypoints.end(),
+             [](const KeyPoint & a, const KeyPoint & b) -> bool
+             {
+                 return a.response > b.response;
+             });
+
+        //keypoints.resize(20);
+
+        map<int, int> h;
         for(auto &k : keypoints) {
-            if (k.size < min)
-                min = k.size;
-            if (k.size > max)
-                max = k.size;
-            avg += k.size;
+            if (h.find(k.size) == h.end())
+                h[k.size] = 1;
+            else
+                h[k.size] += 1;
         }
-        avg /= keypoints.size();
-        std::cout << "min: " << min << " max: " << max << " avg: " << avg << " count: " << keypoints.size() << std::endl;
+
+        for (auto &i : h) {
+            cout << "(" << i.first << ", " << i.second << ")" << endl;
+        }
+
+        cout << endl;
+        cout << endl;
 
         SurfDescriptorExtractor extractor;
         extractor.compute( mat, keypoints, descriptors );
@@ -69,8 +89,8 @@ int main(int argc, char** argv)
     Image img2("../test2.dcm");
     img2.resize(img1);
     //cv::Mat dst;               // dst must be a different Mat
-   // cv::flip(img2.mat, dst, 1);
-    //simg2.mat = dst;
+    //cv::flip(img2.mat, dst, 1);
+    //img2.mat = dst;
     Image img3("../test3.dcm");
     img3.resize(img1);
     Image img4("../test4.dcm");
@@ -78,7 +98,7 @@ int main(int argc, char** argv)
 
 
 
-    cv::SurfFeatureDetector detector{200};
+    cv::SurfFeatureDetector detector{100,16,8};
 
     img1.scan(detector);
     img2.scan(detector);
@@ -105,34 +125,45 @@ int main(int argc, char** argv)
 
 Mat match(const Image &img1, const Image &img2) {//-- Step 3: Matching descriptor vectors using FLANN matcher
     FlannBasedMatcher matcher;
-    vector< DMatch > matches;
-    matcher.match( img1.descriptors, img2.descriptors, matches );
+    vector<DMatch> matches;
+    matcher.match(img1.descriptors, img2.descriptors, matches);
+    vector<DMatch> matches_back;
+    matcher.match(img2.descriptors, img1.descriptors, matches_back);
 
-    double max_dist = 0;
-    double min_dist = 100;
+
+    map<int,int> bm;
+    for (auto &m : matches_back) {
+        bm[m.queryIdx] = m.trainIdx;
+    }
 
 
+    //cout << "m: " << matches.size() << endl;
     //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-    vector< DMatch > good_matches;
+    vector<DMatch> good_matches;
 
     for (auto &matche : matches) {
         auto s1 = img1.keypoints[matche.queryIdx].size;
         auto s2 = img2.keypoints[matche.trainIdx].size;
-        float m = max(s2,s1) / min(s2,s1);
-        if(matche.distance < 0.4 && m < 2)
-        {
+        float m = max(s2, s1) / min(s2, s1);
+        if (matche.distance < 0.3 && m < 1.5 && bm[matche.trainIdx] == matche.queryIdx) {
             good_matches.push_back(matche);
         }
     }
-    //-- Quick calculation of max and min distances between keypoints
-    for (auto &matche : good_matches) {
-        double dist = matche.distance;
-        if( dist < min_dist ) min_dist = dist;
-        if( dist > max_dist ) max_dist = dist;
-    }
 
-    printf("-- Max dist : %f \n", max_dist );
-    printf("-- Min dist : %f \n", min_dist );
+    //-- Quick calculation of max and min distances between keypoints
+    /*
+
+
+    double max_dist = 0;
+    double min_dist = 100;
+     for (auto &matche : good_matches) {
+        double dist = matche.distance;
+        if (dist < min_dist) min_dist = dist;
+        if (dist > max_dist) max_dist = dist;
+    }*/
+
+    //printf("-- Max dist : %f \n", max_dist);
+    //printf("-- Min dist : %f \n", min_dist);
     cout << "gm: " << good_matches.size() << endl;
 
     Mat img_matches;
